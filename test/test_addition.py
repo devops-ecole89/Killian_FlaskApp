46522a139df3285ad85fad3068e1131d6727fd29
Killian <killian.carvalho-pro@protonmail.com>
2024-11-26 17:55:21 +0100

    fix: change architecture for test

--- test/test_addition.py
+++ test/test_addition.py
@@ -1,12 +1,7 @@
 import pytest
-
-def add_numbers(a, b):
-    return a + b
+from addition import add_numbers
 
 def test_add_numbers():
     assert add_numbers(2, 3) == 5
     assert add_numbers(-1, 1) == 0
     assert add_numbers(0, 0) == 0
-
-if test_add_numbers() == 0:
-    print("All tests passed!")
